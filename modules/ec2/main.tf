2ba2a3bea5534692118bea68d1cfc53debdd15b3
Cryptoking94 <98610415+Cryptoking94@users.noreply.github.com>
2024-08-20 21:05:41 +0530

    Create main.tf

---
+++ modules/ec2/main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "this" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  subnet_id     = var.subnet_id
+
+  tags = var.tags
+}
