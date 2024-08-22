c307e15467fab1e4dc814ea94a34520ae7e1cd84
Cryptoking94 <98610415+Cryptoking94@users.noreply.github.com>
2024-08-22 11:56:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ module "ec2_instance" {
   source = "./modules/ec2"
   instance_type = var.instance_type
   ami_id        = var.ami_id
-  key_name      = var.key_name
+  #key_name      = var.key_name
   subnet_id     = var.subnet_id
   tags          = var.ec2_tags
 }
